a6c2559e943293a947cf21065818884f7e6a91b9
Antonis Zalonis <antonis.zalonis@gmail.com>
2022-12-26 18:18:35 +0100

    sign up module

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,17 +2,23 @@ import { NgModule } from '@angular/core';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 @NgModule({
     imports: [
         MatSlideToggleModule,
         MatButtonModule,
         MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
     ],
     exports: [
         MatSlideToggleModule,
         MatButtonModule,
         MatIconModule,
+        MatFormFieldModule,
+        MatInputModule,
     ]
 })
 export class MaterialModule {}
